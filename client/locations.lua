130e151c94a73cca86be9416a51f7d67c32c9684
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2022-08-04 12:06:14 +0200

    Update locations.lua

---
+++ client/locations.lua
@@ -0,0 +1,12 @@
+posizioni = {
+    {
+        pos={x=-1357.514282266, y=-2852.64941406, z=13.8},   --posizione del marker sulla mappa--
+        submarker= {
+            marker = 32,  --submarker--
+            posz = 15.25,
+        },
+        marker= 23,  --marker principale--
+        scale = 4.0,
+        rgba = {120, 255, 120,155}  --colore rgb lascia l'ultimo valore così--
+    },
+}
